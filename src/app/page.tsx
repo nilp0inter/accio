5e5f7a4cc1c24eac220edb461c54cc594f2cab41
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2025-10-27 00:54:32 +0100

    feat: google login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,10 @@ export default function LoginPage() {
       provider: "google",
       options: {
         redirectTo: `${location.origin}/auth/callback`,
+        queryParams: {
+          access_type: 'offline',
+          prompt: 'consent',
+        },
       },
     })
   }
